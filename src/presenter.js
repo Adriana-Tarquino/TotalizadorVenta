c7fd63119efc46c303c08646926186d302762755
Adriana Tarquino <atarquinocespedes@gmail.com>
2022-03-08 23:03:30 -0400

    5.Mostrar el precio neto (cantidad items x precio item)

--- src/presenter.js
+++ src/presenter.js
@@ -11,5 +11,8 @@ form.addEventListener("submit", (event) => {
   const cantidad_2 = cantidad_1.value;
   const precio_2 = precio_1.value;
   const estados_2 = estados_1.value;
-  div.innerHTML = "<p>" + cantidad_2 +"&nbsp"+ precio_2 + "&nbsp" + estados_2 + "&nbsp" + porcentaje_Impuesto(estados_2) +  "</p>";
+  //div.innerHTML = "<p>" + cantidad_2 +"&nbsp"+ precio_2 + "&nbsp" + estados_2 + "&nbsp" + porcentaje_Impuesto(estados_2) +  "</p>";
+  div.innerHTML = "<p>" + cantidad_2 * precio_2 + "</p>";
+
+
 });
