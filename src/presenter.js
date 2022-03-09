8efd4a72038b3537b9962bf664a7fa0fb31c8bad
Adriana Tarquino <atarquinocespedes@gmail.com>
2022-03-08 23:00:44 -0400

    4.Seleccionar estado y mostrar el porcentaje de impuesto que tiene ese estado

--- src/presenter.js
+++ src/presenter.js
@@ -1,3 +1,5 @@
+import porcentaje_Impuesto from "./calculos";
+
 const cantidad_1 = document.querySelector("#cantidad");
 const precio_1 = document.querySelector("#precio");
 const div = document.querySelector("#resul");
@@ -9,5 +11,5 @@ form.addEventListener("submit", (event) => {
   const cantidad_2 = cantidad_1.value;
   const precio_2 = precio_1.value;
   const estados_2 = estados_1.value;
-  div.innerHTML = "<p>" + cantidad_2 +"&nbsp"+ precio_2 + "&nbsp" + estados_2 + "</p>";
+  div.innerHTML = "<p>" + cantidad_2 +"&nbsp"+ precio_2 + "&nbsp" + estados_2 + "&nbsp" + porcentaje_Impuesto(estados_2) +  "</p>";
 });
