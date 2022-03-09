1c8aeba2f572daf0bc3b4c5518dc9934dec3ed16
Adriana Tarquino <atarquinocespedes@gmail.com>
2022-03-08 22:42:20 -0400

    3.Seleccionar el estado y mostrar eleccion

--- src/presenter.js
+++ src/presenter.js
@@ -1,12 +1,13 @@
 const cantidad_1 = document.querySelector("#cantidad");
 const precio_1 = document.querySelector("#precio");
 const div = document.querySelector("#resul");
+const estados_1 = document.querySelector("#estados");
 const form = document.querySelector("#ventas-form");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
   const cantidad_2 = cantidad_1.value;
   const precio_2 = precio_1.value;
-  div.innerHTML = "<p>" + cantidad_2 +"&nbsp"+ precio_2 + "</p>";
-
+  const estados_2 = estados_1.value;
+  div.innerHTML = "<p>" + cantidad_2 +"&nbsp"+ precio_2 + "&nbsp" + estados_2 + "</p>";
 });
