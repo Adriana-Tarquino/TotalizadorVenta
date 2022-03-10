90689b5f1119d799ea02c886a05392077257995d
Adriana Tarquino <atarquinocespedes@gmail.com>
2022-03-10 02:48:55 -0400

    12. Mostrar el precio total con el Porcentaje de descuento que corresponde a 3000

--- src/calculos3.js
+++ src/calculos3.js
@@ -1,30 +1,16 @@
 function porcentaje_Descuento(a)
 {
     var resp = 0;
-    /*switch (a) {
-        case a >= 1000 || a < 3000:
-            resp = 0.03;
-            break;
-        case a >= 3000 && a < 7000:
-            resp = 0.05;
-            break;
-        case a >= 7000 && a < 10000:
-            resp = 0.07;
-            break;
-        case a >= 10000 && a < 30000:
-            resp = 0.10; 
-            break;
-        case a >= 30000:
-            resp = 0.15;
-            break;
-        default:
-            break;
-    }*/
-    if(a >= 1000 || a < 3000)
+    if(a >= 1000 && a < 3000)
     {
-        resp = 3;
+        resp = 3;    
+    }
+    if(a >= 3000 && a < 7000)
+    {
+       resp = 5;
     }
     
+       
     return resp;
 }
 
